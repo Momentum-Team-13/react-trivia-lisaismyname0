0125f04fa217e9497a40277a74fec72d0825ad7e
Lisa Williams <lisaismyname0@gmail.com>
2022-07-20 12:59:32 -0400

    user answers .push to empty array

--- trivia/src/components/categories.js
+++ trivia/src/components/categories.js
@@ -6,6 +6,7 @@ export default function Categories(){
     const [selectedCategory, setSelectedCategory] = useState(null)
     const [categoryURL, setCategoryURL] = useState(`https://opentdb.com/api.php?amount=10&category=${selectedCategory}`)
     const [triviaQuestions, setTriviaQuestions] = useState([])
+    const userAnswerBank = []
 
     const handleSelectedCategory=(props)=>{
     // this creates the URL based off of the ID of the category
@@ -14,6 +15,11 @@ export default function Categories(){
         setCategoryURL(`https://opentdb.com/api.php?amount=10&category=${categoryID}`)
     }
 
+    const handleUserAnswer = (props) =>{
+        let userAnswer = props
+        userAnswerBank.push(userAnswer)
+        console.log(userAnswerBank)
+    }
 
     useEffect(() => {
         // to make ajax call that will return list of trivia categories that i will then use to populate my dropdown menu
@@ -40,18 +46,17 @@ export default function Categories(){
     </select>
 
     {triviaQuestions ? 
-    
     <div>
     {triviaQuestions.map ((question)=> (
-        <p key={question.question}> {question.question}
-        <br/>
+        <div key ={question.question}>
+        <p key={question.question}> {question.question}</p>
         <br/>
         <div className ="buttons">
-        <button onClick={()=> console.log({question})} key={question.correct_answer}>{question.correct_answer}</button>
+        <button onClick={(e)=> handleUserAnswer(e.target.textContent)} key={question.correct_answer}>{question.correct_answer}</button>
         {question.incorrect_answers.map((answer) => (
-            <button key={answer.incorrect_answers}>{answer} </button>
+            <button onClick={(e)=> handleUserAnswer(e.target.textContent)} key={answer.incorrect_answers}>{answer} </button>
         ))}</div>
-        </p>
+        </div>
     ))}
     </div>: ("")}
     </>
