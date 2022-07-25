86d81623a8397c15f347ee32d342e5574f01fc7e
Lisa Williams <lisaismyname0@gmail.com>
2022-07-25 11:12:46 -0400

    added start over button to final page

--- trivia/src/components/questionPage.js
+++ trivia/src/components/questionPage.js
@@ -1,29 +1,38 @@
-import { useState } from 'react'
-import OneQuestion from './oneQuestion'
+import { useState } from "react";
+import OneQuestion from "./oneQuestion";
 
-export default function QuestionPage ({triviaQuestions, correctAnswerBank, shuffleButtons,}){
+export default function QuestionPage({
+  triviaQuestions,
+  correctAnswerBank,
+  shuffleButtons,
+}) {
+  const [start, setStart] = useState(true);
+  const [index, setIndex] = useState(0);
+  const [correct, setCorrect] = useState(false);
+  const [incorrect, setIncorrect] = useState(false);
 
-
-    const [start, setStart] = useState(true)
-    const [index, setIndex] = useState(0)
-    const [correct, setCorrect] = useState(false)
-    const [incorrect, setIncorrect] = useState(false)
-
-return(
-
+  return (
     <>
-    { start ? (<>
-        <Index index={index}/>
-        
-        <OneQuestion index={index} questions ={triviaQuestions} setIndex={() => setIndex(index+1)} correctAnswerBank={correctAnswerBank} shuffleButtons={shuffleButtons}/>
-    </>) : ("Goodbye") }
+      {start ? (
+        <>
+          <Index index={index} />
 
+          <OneQuestion
+            index={index}
+            questions={triviaQuestions}
+            setIndex={() => setIndex(index + 1)}
+            correctAnswerBank={correctAnswerBank}
+            shuffleButtons={shuffleButtons}
+            setStart={() => setStart(true)}
+          />
+        </>
+      ) : (
+        "Goodbye"
+      )}
     </>
-)
+  );
 }
 
-const Index = ({index}) => {
-    return(
-    <></>
-    )
-}
+const Index = ({ index }) => {
+  return <></>;
+};
